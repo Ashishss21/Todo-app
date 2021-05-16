122b6c0a59af96d5b2a976c5886bcdc93a304e4c
Ashish Sengar <65999078+Ashishss21@users.noreply.github.com>
2021-05-16 14:53:21 +0530

    Production build

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import React, { useState } from 'react';
-import './App.css';
-import {Button, FormControl, Input, InputLabel} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import Todo from './Todo';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import './App.css';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
-
-  const [todos, setTodo] = useState(['Hello everyone','Take dogs for a walk','Hii... everyone']);
-
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+  
+  //when the app koads, we need to listen to the database and fetch new todos as the get added/removed
+  useEffect(() => {
+    //this code here.... fires when the app.js loads
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
+    })
+  }, []);
 
-  const addTodo= (event) =>{
-    //Add todo using useState and setState 
-    event.preventDefault()
-    console.log('hello')
-    setTodo([...todos, input])
-    setInput('')
+  const addTodo = (event) => {
+    //this will fie off when we click the button
+    event.preventDefault(); //will stop the REFRESH
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    
+    setInput(''); //clear up the input after clicking add todo button
   }
 
   return (
     <div className="App">
-        <h1>Hello World!!</h1>
+      <h1>Hello Todo App ✅!</h1>
+      
+      <form>
+        <FormControl>
+          <InputLabel>✅Write a Todo</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+        </FormControl>
 
-        <form>
-          <FormControl>
-            <InputLabel>Add a new task</InputLabel>
-            <Input value={input} onChange={(event)=> setInput(event.target.value)} />
-            <Button disabled={!input} onClick={addTodo} variant="contained" color="primary">
-            Add TO-DO
-            </Button>
-          </FormControl>
-        </form>
-        
-        <ul>
-            {todos.map(todo => {
-              return(
-                <Todo list={todo}/>
-              );
-            })}
-        </ul>
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
+      </form>
+
+      <ul>
+        {todos.map(todo => (
+          <Todo todo={todo} />
+          //<li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
